8b2cb8a135f4cb565c5ffb928f575a442a795927
Manisha <manisha_msathe@yahoo.com>
2020-04-06 09:45:11 +0530

    swagger documentation related changes

--- src/routes/health-check.routes.js
+++ src/routes/health-check.routes.js
@@ -3,6 +3,20 @@ const healthcheckCotroller = require('../controllers/health-check.controller');
 const errorHandler = require('../shared/error-handler');
 
 const healthcheckRoutes = express.Router();
+
+/**
+ * @swagger
+ *
+ * /healthcheck:
+ *   get:
+ *     description: Health check API specs
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *          description: healthcheck response message
+ *
+ */
 healthcheckRoutes.get('/', errorHandler.handleAsyncError(healthcheckCotroller.getHealthcheckResponse));
 
 module.exports = healthcheckRoutes;
